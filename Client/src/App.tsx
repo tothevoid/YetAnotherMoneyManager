a376e97c2c204e69a534cd67bbf31c6f777a3d27
tothevoid <tothevoid42@gmail.com>
2025-02-24 20:49:58 +0300

    added routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,13 +2,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Manager from './pages/Manager/Manager'
 import Footer from './components/Footer/Footer';
+import DepositsPage from './pages/DepositsPage/DepositsPage';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app">
       <Header/>
       <main className="app-main">
-        <Manager></Manager>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Manager />} />
+            <Route path="/deposits" element={<DepositsPage />} />
+          </Routes>
+        </Router>
       </main>
       <Footer/>
     </div>

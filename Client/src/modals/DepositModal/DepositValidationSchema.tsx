994aaa0bf608d8889bd7e8dd1669e7e868a62f74
tothevoid <tothevoid42@gmail.com>
2025-03-11 00:27:50 +0300

    added dates range validation

--- Client/src/modals/DepositModal/DepositValidationSchema.tsx
+++ Client/src/modals/DepositModal/DepositValidationSchema.tsx
@@ -4,17 +4,25 @@ export type DepositFormInput = z.infer<typeof depositValidationSchema>;
 
 const depositValidationSchema = z.object({
   id: string().optional(),
-  name: z.string().min(1, { message: 'Name should be at least one symbol' }),
+  name: z.string().min(1),
   percentage: z.number()
     .nonnegative()
-    .min(0)
+    .gt(0)
     .max(100),
   initialAmount: z.number()
     .nonnegative()
-    .min(0, { message: 'Initial amount should be more than zero' }),
-  from: z.date().refine(date => date),
+    .gt(0),
+  from: z.date(),
   to: z.date(),
-});
+})
+.refine(({from, to}) => to > from, {
+  message: "End date must be later than start date",
+  path: ["to"],
+})
+.refine(({from, to}) => from < to, {
+  message: "Start date must be earlier than end date",
+  path: ["from"],
+});;
 
 
 export default depositValidationSchema;

8682324792e70f5732b53c069b2076138a9a0b6d
tothevoid <tothevoid42@gmail.com>
2025-03-14 02:10:54 +0300

    added basic localization

--- Client/src/components/Header/Header.tsx
+++ Client/src/components/Header/Header.tsx
@@ -1,10 +1,30 @@
 import './Header.scss';
 import HeaderItem from '../HeaderItem/HeaderItem';
+import { Button, Spacer } from '@chakra-ui/react';
+import { changeLanguage } from 'i18next';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Header = () => 
-    <nav className="header">
-        <HeaderItem title="Manager" path='/'></HeaderItem>
-        <HeaderItem title="Deposits" path='/Deposits'></HeaderItem>
-    </nav>
+const Header = () => {
+    const { t, i18n } = useTranslation();
+    const [language, setLanguage] = useState(i18n.language);
+
+    const updateLanguage = () => {
+        setLanguage(i18n.language);
+        const nextLanguage = i18n.language === "en" ? "ru" : "en";
+        localStorage.setItem("lang", nextLanguage);
+        changeLanguage(nextLanguage)
+    }
+
+    return <div className="header">
+        <HeaderItem title={t("title_manager")} path='/'></HeaderItem>
+        <HeaderItem title={t("title_deposits")} path='/Deposits'></HeaderItem>
+        <Spacer/>
+        <Button onClick={() => updateLanguage()} background={"gray.400"}>
+            {language}
+        </Button>
+    </div>
+}
+   
 
 export default Header;

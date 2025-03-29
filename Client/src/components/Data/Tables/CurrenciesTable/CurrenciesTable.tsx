ed633d8ac6f2d9c53e15e873bc70267759979003
tothevoid <tothevoid42@gmail.com>
2025-03-29 16:06:43 +0300

    added currency removal

--- Client/src/components/Data/Tables/CurrenciesTable/CurrenciesTable.tsx
+++ Client/src/components/Data/Tables/CurrenciesTable/CurrenciesTable.tsx
@@ -1,22 +1,26 @@
 import { Box, Button, Checkbox, Icon, Input, Table } from "@chakra-ui/react";
 import { CurrencyEntity } from "../../../../models/CurrencyEntity";
 import { useEffect, useRef, useState } from "react";
-import { createCurrency, getCurrencies, updateCurrency } from "../../../../api/currencyApi";
-import { MdAdd } from "react-icons/md";
+import { createCurrency, deleteCurrency, getCurrencies, updateCurrency } from "../../../../api/currencyApi";
+import { MdAdd, MdDelete } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 import CurrencyModal, { CurrencyModalRef } from "../../../../modals/CurrencyModal/CurrencyModal";
+import { ConfirmModal, ConfirmModalRef } from "../../../../modals/ConfirmModal/ConfirmModal";
 
 interface Props {
 }
 
 interface State {
     currencies: CurrencyEntity[],
-    hasChanges: boolean
+    hasChanges: boolean,
+    currentCurrencyId: string | null
 }
 
 const CurrenciesTable: React.FC<Props> = () => {
-    const [state, setState] = useState<State>({currencies: [], hasChanges: false});
+    const [state, setState] = useState<State>({currencies: [], hasChanges: false, currentCurrencyId: null});
     const { t } = useTranslation();
+    const modalRef = useRef<CurrencyModalRef>(null);
+    const confirmModalRef = useRef<ConfirmModalRef>(null);
 
     useEffect(() => {
         const initData = async () => { 
@@ -70,8 +74,6 @@ const CurrenciesTable: React.FC<Props> = () => {
         await updateCurrency({...currency});
     }
     
-    const modalRef = useRef<CurrencyModalRef>(null);
-    
     const onAdd = () => {
         modalRef.current?.openModal()
     };
@@ -89,12 +91,42 @@ const CurrenciesTable: React.FC<Props> = () => {
         })
     };
 
+    const onDeleteClicked = async (currency: CurrencyEntity) => {
+        setState((currentState) => {
+            return {...currentState, currentCurrencyId: currency.id}
+        })
+        confirmModalRef.current?.openModal()
+    }
+
+    const onDeleteConfirmed = async () => {
+        const {currentCurrencyId} = state;
+
+        if (!currentCurrencyId){
+            return;
+        }
+
+        const isDeleted = await deleteCurrency(currentCurrencyId);
+        
+        if (!isDeleted) {
+            return;
+        }
+
+        const currencies = state.currencies.filter((currency: CurrencyEntity) => {
+            return currency.id !== state.currentCurrencyId;
+        });
+
+        setState((currentState) => {
+            return {...currentState, currencies, currentCurrencyId: null}
+        })
+    }
+
     return <Box color="text_primary">
         <Table.Root>
             <Table.Header>
                 <Table.Row border="none" bg="none" color="text_primary">
                     <Table.ColumnHeader color="text_primary">Name</Table.ColumnHeader>
                     <Table.ColumnHeader color="text_primary">Active</Table.ColumnHeader>
+                    <Table.ColumnHeader color="text_primary">Delete</Table.ColumnHeader>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
@@ -106,29 +138,40 @@ const CurrenciesTable: React.FC<Props> = () => {
                                     onChange={(handler) => onCellChanged(currency.id, "name", handler.target.value)}>
                                 </Input>
                             </Table.Cell>
-                            <Table.Cell>
+                            <Table.Cell width={10}>
                                 <Checkbox.Root onBlur={() => onCellBlur(currency.id)} checked={currency.active} variant="subtle"
                                     onCheckedChange={(data) => {onCellChanged(currency.id, "active", data.checked)}}>
                                     <Checkbox.HiddenInput />
                                     <Checkbox.Control />
                                 </Checkbox.Root>
                             </Table.Cell>
+                            <Table.Cell width={10}>
+                                <Button borderColor="background_secondary" background="button_background_secondary" size={'sm'} onClick={() => onDeleteClicked(currency)}>
+                                    <Icon color="card_action_icon_danger">
+                                        <MdDelete/>
+                                    </Icon>
+                                </Button>
+                            </Table.Cell>
                         </Table.Row>
                     })
                 }
             </Table.Body>
-            <Table.Footer padding={22}>
-                <Box padding={4}>
-                    <Button background="purple.600" onClick={onAdd}>
-                        <Icon size='md'>
-                            <MdAdd/>
-                        </Icon>
-                        {t("currencies_data_add")}
-                    </Button>
-                </Box>
-            </Table.Footer>
         </Table.Root>
+        <Box padding={4}>
+            <Button background="purple.600" onClick={onAdd}>
+                <Icon size='md'>
+                    <MdAdd/>
+                </Icon>
+                {t("currencies_data_add")}
+            </Button>
+        </Box>
         <CurrencyModal ref={modalRef} onSaved={onCurrencyAdded}></CurrencyModal>
+        <ConfirmModal onConfirmed={onDeleteConfirmed}
+                    title={t("currencies_delete_title")}
+                    message={t("modals_delete_message")}
+                    confirmActionName={t("modals_delete_button")}
+                    ref={confirmModalRef}>
+        </ConfirmModal>
     </Box>
 }
 

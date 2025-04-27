da6ecbcbc852d5993dca3f55e9e71650480c1528
tothevoid <tothevoid42@gmail.com>
2025-04-28 00:30:54 +0300

    removed ability to add broker account securities

--- Client/src/components/brokers/BrokerAccountSecuritiesList/BrokerAccountSecuritiesList.tsx
+++ Client/src/components/brokers/BrokerAccountSecuritiesList/BrokerAccountSecuritiesList.tsx
@@ -1,10 +1,8 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { SimpleGrid } from '@chakra-ui/react/grid';
-import { Flex } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import BrokerAccountSecurity from '../BrokerAccountSecurity/BrokerAccountSecurity';
 import { BrokerAccountSecurityEntity } from '../../../models/brokers/BrokerAccountSecurityEntity';
-import AddBrokerAccountSecurityButton from '../AddBrokerAccountSecurityButton/AddBrokerAccountSecurityButton';
 import { getSecuritiesByBrokerAccount } from '../../../api/brokers/brokerAccountSecurityApi';
 
 interface Props {
@@ -64,9 +62,6 @@ const BrokerAccountSecuritiesList: React.FC<Props> = (props) => {
 
     return (
         <Fragment>
-            <Flex justifyContent="space-between" alignItems="center" pt={5} pb={5}>
-                <AddBrokerAccountSecurityButton onAdded={onBrokerAccountSecurityAdded}></AddBrokerAccountSecurityButton>
-            </Flex>
             <SimpleGrid pt={5} pb={5} gap={4} templateColumns='repeat(auto-fill, minmax(400px, 3fr))'>
                 {
                 state.brokerAccountSecurities.map((brokerAccountSecurity: BrokerAccountSecurityEntity) => {

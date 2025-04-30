3c97d8dbde3992998bcb7427880c36d828bb226e
tothevoid <tothevoid42@gmail.com>
2025-05-01 00:30:04 +0300

    added migrations

---
+++ Server/Infrastructure/Migrations/20250430183903_init.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Broker",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Broker", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrokerAccountType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrokerAccountType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currency",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currency", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Extension = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    CurrencyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateOnly>(type: "date", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_AccountType_AccountTypeId",
+                        column: x => x.AccountTypeId,
+                        principalTable: "AccountType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Account_Currency_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrokerAccount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    TypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CurrencyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BrokerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastUpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AssetsValue = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrokerAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BrokerAccount_BrokerAccountType_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "BrokerAccountType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BrokerAccount_Broker_BrokerId",
+                        column: x => x.BrokerId,
+                        principalTable: "Broker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BrokerAccount_Currency_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Security",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Ticker = table.Column<string>(type: "text", nullable: true),
+                    TypeId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Security", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Security_SecurityType_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "SecurityType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deposit",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    From = table.Column<DateOnly>(type: "date", nullable: false),
+                    To = table.Column<DateOnly>(type: "date", nullable: false),
+                    Percentage = table.Column<decimal>(type: "numeric", nullable: false),
+                    InitialAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    EstimatedEarn = table.Column<decimal>(type: "numeric", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deposit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Deposit_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    MoneyQuantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TransactionType = table.Column<string>(type: "text", nullable: true),
+                    TransactionTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cashback = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsSystem = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transaction_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrokerAccountSecurity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BrokerAccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SecurityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    InitialPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    CurrentPrice = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrokerAccountSecurity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BrokerAccountSecurity_BrokerAccount_BrokerAccountId",
+                        column: x => x.BrokerAccountId,
+                        principalTable: "BrokerAccount",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BrokerAccountSecurity_Security_SecurityId",
+                        column: x => x.SecurityId,
+                        principalTable: "Security",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityTransaction",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SecurityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BrokerAccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Commission = table.Column<decimal>(type: "numeric", nullable: false),
+                    Tax = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityTransaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurityTransaction_BrokerAccount_BrokerAccountId",
+                        column: x => x.BrokerAccountId,
+                        principalTable: "BrokerAccount",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SecurityTransaction_Security_SecurityId",
+                        column: x => x.SecurityId,
+                        principalTable: "Security",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_AccountTypeId",
+                table: "Account",
+                column: "AccountTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_CurrencyId",
+                table: "Account",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokerAccount_BrokerId",
+                table: "BrokerAccount",
+                column: "BrokerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokerAccount_CurrencyId",
+                table: "BrokerAccount",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokerAccount_TypeId",
+                table: "BrokerAccount",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokerAccountSecurity_BrokerAccountId",
+                table: "BrokerAccountSecurity",
+                column: "BrokerAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokerAccountSecurity_SecurityId",
+                table: "BrokerAccountSecurity",
+                column: "SecurityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deposit_AccountId",
+                table: "Deposit",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Security_TypeId",
+                table: "Security",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityTransaction_BrokerAccountId",
+                table: "SecurityTransaction",
+                column: "BrokerAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityTransaction_SecurityId",
+                table: "SecurityTransaction",
+                column: "SecurityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_AccountId",
+                table: "Transaction",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrokerAccountSecurity");
+
+            migrationBuilder.DropTable(
+                name: "Deposit");
+
+            migrationBuilder.DropTable(
+                name: "SecurityTransaction");
+
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "TransactionType");
+
+            migrationBuilder.DropTable(
+                name: "BrokerAccount");
+
+            migrationBuilder.DropTable(
+                name: "Security");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "BrokerAccountType");
+
+            migrationBuilder.DropTable(
+                name: "Broker");
+
+            migrationBuilder.DropTable(
+                name: "SecurityType");
+
+            migrationBuilder.DropTable(
+                name: "AccountType");
+
+            migrationBuilder.DropTable(
+                name: "Currency");
+        }
+    }
+}

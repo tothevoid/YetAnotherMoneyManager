685f96243d0db0160717eccf9e96193b0cfd27d8
tothevoid <tothevoid42@gmail.com>
2025-05-22 20:20:04 +0300

    added user profile migrations

---
+++ Server/Infrastructure/Migrations/20250522171938_AddedUserProfile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CurrencyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LanguageCode = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfile_Currency_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserProfile",
+                columns: new[] { "Id", "CurrencyId", "LanguageCode" },
+                values: new object[] { new Guid("9e2104a4-4b8d-4d4c-8585-0b24cf11e891"), new Guid("c7f31af3-5091-439c-8854-c90872420ecf"), "EN" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfile_CurrencyId",
+                table: "UserProfile",
+                column: "CurrencyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfile");
+        }
+    }
+}

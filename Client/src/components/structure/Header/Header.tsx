50a4fb79ddd810f48342e6b299a2836e778661bf
tothevoid <tothevoid42@gmail.com>
2025-05-25 16:23:23 +0300

    reworked header

--- Client/src/components/structure/Header/Header.tsx
+++ Client/src/components/structure/Header/Header.tsx
@@ -1,5 +1,5 @@
 import HeaderItem from '../HeaderItem/HeaderItem';
-import { Box, Button,Flex, Icon, Text } from '@chakra-ui/react';
+import { Box, Button,Flex, Icon, Image } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { MdOutlineSettings } from 'react-icons/md';
@@ -17,11 +17,10 @@ const Header = () => {
         <Box pos="fixed" w={"100%"} zIndex={1000}>
             <Flex minH={50} alignItems="center" padding={1} direction={'row'} backgroundColor="header_bg" color="text_primary">
                 <Flex flex={{ base: 1 }} justify="center" align={"center"}>
-                    <Text fontWeight={900} textAlign="center">
-                       YAMM
-                    </Text>
+                    <Image width="30px" src="src/assets/icon.svg"></Image>
                     <Flex flex="1">
-                        <HeaderItem title={t("header_accounts")} path='/'></HeaderItem>
+                        <HeaderItem title={t("header_dashboard")} path='/'></HeaderItem>
+                        <HeaderItem title={t("header_accounts")} path='/accounts'></HeaderItem>
                         <HeaderItem title={t("header_transactions")} path='/transactions'></HeaderItem>
                         <HeaderItem title={t("header_deposits")} path='/deposits'></HeaderItem>
                         <HeaderItem title={t("header_broker_account")} path='/broker_accounts'></HeaderItem>
@@ -30,7 +29,7 @@ const Header = () => {
                     </Flex>
                 </Flex>
                 <Flex flex="1" justify="flex-end" direction="row">
-                    <Button borderColor="background_secondary" background="button_background_secondary" size={'2xl'} onClick={onOpenSettingsClick}>
+                    <Button borderColor="background_secondary" background="button_background_secondary" size={'md'} onClick={onOpenSettingsClick}>
                         <Icon color="card_action_icon_primary">
                             <MdOutlineSettings/>
                         </Icon>

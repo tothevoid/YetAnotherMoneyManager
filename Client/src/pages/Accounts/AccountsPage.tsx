379f27fd81805b90ae3f4dacd7e6fe2f0a2aa78e
tothevoid <tothevoid42@gmail.com>
2025-06-08 14:32:26 +0300

    added accounts hook

--- Client/src/pages/Accounts/AccountsPage.tsx
+++ Client/src/pages/Accounts/AccountsPage.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useState } from "react";
 import { getSummary } from "../../api/accounts/accountApi";
 import { AccountCurrencySummary } from "../../models/accounts/accountsSummary";
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { formatMoneyByCurrencyCulture } from "../../shared/utilities/formatters/moneyFormatter";
 import { useTranslation } from "react-i18next";
 import AccountsList from "./components/AccountsList/AccountsList";
@@ -9,37 +9,36 @@ import AccountsList from "./components/AccountsList/AccountsList";
 interface Props {}
 
 interface State {
-    accountCurrencySummaries: AccountCurrencySummary[]
+	accountCurrencySummaries: AccountCurrencySummary[]
 }
 
 const AccountsPage: React.FC<Props> = () => {
-    const [state, setState] = useState<State>({accountCurrencySummaries: []});
+	const [state, setState] = useState<State>({accountCurrencySummaries: []});
 
-    const { t } = useTranslation();
-    const requestAccountsData = async () => {
-        const accountCurrencySummaries = await getSummary();
-        setState((currentState) => {
-            return {...currentState, accountCurrencySummaries}
-        })
-    };
+	const { t } = useTranslation();
+	const requestAccountsData = async () => {
+		const accountCurrencySummaries = await getSummary();
+		setState((currentState) => {
+			return {...currentState, accountCurrencySummaries}
+		})
+	};
 
-    const onAccountsChanged = async () => {
-        await requestAccountsData();
-    }
+	const onAccountsChanged = async () => {
+		await requestAccountsData();
+	}
 
-    return (<Fragment>
-        {
-            state.accountCurrencySummaries.length > 0 ?
-                <Flex justifyContent="space-between" alignItems="center" pt={5} pb={5}>
-                    <Text fontSize='3xl'>{t("accounts_page_summary_title")}: {state.accountCurrencySummaries.map(currency => 
-                        formatMoneyByCurrencyCulture(currency.summary, currency.name)).join(" | ") }
-                    </Text>
-                </Flex>:
-                <Fragment/>
-        }
-        <AccountsList onAccountsChanged={onAccountsChanged}/>
-    </Fragment>
-    )
+	return <Box>
+		{
+			state.accountCurrencySummaries.length > 0 ?
+				<Flex justifyContent="space-between" alignItems="center" pt={5} pb={5}>
+					<Text fontSize='3xl'>{t("accounts_page_summary_title")}: {state.accountCurrencySummaries.map(currency => 
+						formatMoneyByCurrencyCulture(currency.summary, currency.name)).join(" | ") }
+					</Text>
+				</Flex>:
+				<Fragment/>
+		}
+		<AccountsList onAccountsChanged={onAccountsChanged}/>
+	</Box>
 }
 
 export default AccountsPage;
---
+++ Client/src/pages/Accounts/hooks/useAccounts.ts
@@ -0,0 +1,77 @@
+import { useCallback, useEffect, useState } from "react";
+import { AccountEntity } from "../../../models/accounts/AccountEntity";
+import { createAccount, getAccounts } from "../../../api/accounts/accountApi";
+
+export interface AccountsQuery {
+	onlyActive: boolean
+}
+
+export const useAccounts = (queryParameters: AccountsQuery) => {
+	const [accounts, setAccounts] = useState<AccountEntity[]>([]);
+	const [isAccountsLoading, setLoading] = useState(false);
+
+	const [error, setError] = useState<string | null>(null);
+	const [accountQueryParameters, setAccountQueryParameters] = useState<AccountsQuery>(queryParameters);
+
+	const fetchData = useCallback(async () => {
+		setLoading(true)
+		try {
+			const accounts = await getAccounts(accountQueryParameters.onlyActive);
+			setAccounts(accounts);
+		} catch (err: any) {
+			setError(err.message || 'Ошибка загрузки данных')
+		} finally {
+			setLoading(false)
+		}
+	}, [accountQueryParameters])
+
+	useEffect(() => {
+		fetchData();
+	}, [fetchData])
+
+	const createAccountEntity = async (account: AccountEntity) => {
+		const createdAccountId = await createAccount(account);
+		if (!createdAccountId) {
+			return;
+		}
+
+		account.id = createdAccountId;
+		setAccounts([...accounts, account]);
+	}
+
+	const updateAccountEntity = async (updatedAccount: AccountEntity) => {
+	   if (!updatedAccount) {
+			return;
+		}
+
+		const updatedAccounts = accounts.map((account: AccountEntity) => {
+			if (account.id === updatedAccount.id) {
+				return {...updatedAccount}
+			} 
+			return account
+		});
+
+		setAccounts(updatedAccounts)
+	}
+
+	const deleteAccountEntity = async (deletedAccount: AccountEntity) => {
+		if (!deletedAccount) {
+			return;
+		}
+
+		setAccounts(accounts.filter((account: AccountEntity) => account.id !== deletedAccount.id));
+	}
+	
+
+	return {
+		accounts,
+		isAccountsLoading,
+		error,
+		createAccountEntity,
+		updateAccountEntity,
+		deleteAccountEntity,
+		setAccountQueryParameters: setAccountQueryParameters,
+		accountQueryParameters: accountQueryParameters,
+		reloadAccounts: fetchData
+	}
+}

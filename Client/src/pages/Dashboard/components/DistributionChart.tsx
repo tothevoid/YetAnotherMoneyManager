11d73686a6b146f8c3a4276e0e9e6b4611190a60
tothevoid <tothevoid42@gmail.com>
2025-06-15 13:27:57 +0300

    added dashboard percentage, fixed title

--- Client/src/pages/Dashboard/components/DistributionChart.tsx
+++ Client/src/pages/Dashboard/components/DistributionChart.tsx
@@ -47,7 +47,13 @@ const DistributionChart = (props: Props) => {
                     }
                     </Pie>
                     <Tooltip contentStyle={getChartLabelConfig()} itemStyle={{ color: "#E0E0E0" }} formatter={formatLabel}/>
-                    <Legend />
+                    <Legend
+                        formatter={(value, entry, index) => {
+                            const total = props.data.reduce((sum, item) => sum + item.convertedAmount, 0);
+                            const percent = ((props.data[index].convertedAmount / total) * 100).toFixed(1);
+                            return `${value} (${percent}%)`;
+                        }}
+                    />
                 </PieChart>
             </ResponsiveContainer>
         </div>

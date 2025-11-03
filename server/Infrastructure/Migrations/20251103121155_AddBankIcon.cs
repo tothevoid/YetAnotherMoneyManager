d21d6d6e07cbde5592f19843294eff0ff01d574d
tothevoid <tothevoid42@gmail.com>
2025-11-03 15:20:17 +0300

    added bank icon migration

---
+++ server/Infrastructure/Migrations/20251103121155_AddBankIcon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBankIcon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconKey",
+                table: "Bank",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconKey",
+                table: "Bank");
+        }
+    }
+}

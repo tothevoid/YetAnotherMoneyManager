ff97039b83851335d700f11cc69c2392ff476d31
tothevoid <tothevoid42@gmail.com>
2026-02-08 20:07:10 +0300

    added diff color calculation func

--- client/src/shared/utilities/numericDiffsUtilities.ts
+++ client/src/shared/utilities/numericDiffsUtilities.ts
@@ -1,21 +1,26 @@
 import { formatMoneyByCurrencyCulture } from "./formatters/moneyFormatter";
 import { Nullable } from "./nullable";
 
+export const  getDiffColor = (profitAndLoss: number): string => {
+    if (profitAndLoss > 0) return "green.600";
+    if (profitAndLoss < 0) return "red.600";
+    return "text_primary";
+}
+
 export const calculateDiff = (currentValue: number, initialValue: number, currency: Nullable<string> = null) => {
     const profitAndLoss = currentValue - initialValue;
     const profitAndLossPercentage = initialValue ? 
         profitAndLoss / initialValue * 100:
         0;
-    
-    const color = profitAndLoss > 0 ?
-        "green.600":
-        "red.600";
+
+    const color = getDiffColor(profitAndLoss);
 
     const formattedProfitAndLoss = currency ?
         formatMoneyByCurrencyCulture(profitAndLoss, currency):
         profitAndLoss.toFixed(2)
 
-    return {profitAndLoss: formattedProfitAndLoss, 
+    return {
+        profitAndLoss: formattedProfitAndLoss, 
         profitAndLossPercentage: profitAndLossPercentage.toFixed(2),
         rawProfitAndLoss: profitAndLoss,
         color

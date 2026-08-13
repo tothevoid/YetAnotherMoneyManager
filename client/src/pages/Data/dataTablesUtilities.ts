4d840746f03ecb8468b8908b037d0e5065b9157b
tothevoid <tothevoid42@gmail.com>
2026-08-14 02:30:28 +0300

    added sidebar lcz

--- client/src/pages/Data/dataTablesUtilities.ts
+++ client/src/pages/Data/dataTablesUtilities.ts
@@ -1,20 +1,20 @@
-interface DataTable {
-    id: string,
-    href: string,
-    label: string
+import { TFunction } from "i18next";
+
+export interface DataTable {
+    id: string;
+    href: string;
+    label: string;
 }
 
-export const getDataTablesConfig = (): DataTable[] => {
+export const getDataTablesConfig = (t: TFunction): DataTable[] => {
     const basicPath = "data";
 
-    //TODO add i18n
-
     return [
-        { id: "transaction_types", href: `/${basicPath}/transaction_types`, label: "Типы транзакций" },
-        { id: "currencies", href: `/${basicPath}/currencies`, label: "Валюты" },
-        { id: "banks", href: `/${basicPath}/banks`, label: "Банки" },
-        { id: "broker_account_types", href: `/${basicPath}/broker_account_types`, label: "Типы брокерских счетов" },
-        { id: "brokers", href: `/${basicPath}/brokers`, label: "Брокеры" },
-        { id: "crypto_providers", href: `/${basicPath}/crypto_providers`, label: "Крипто провайдеры" }
-    ]
-}
+        { id: "transaction_types", href: `/${basicPath}/transaction_types`, label: t("data_tab_transaction_types") },
+        { id: "currencies", href: `/${basicPath}/currencies`, label: t("data_tab_currencies") },
+        { id: "banks", href: `/${basicPath}/banks`, label: t("data_tab_banks") },
+        { id: "broker_account_types", href: `/${basicPath}/broker_account_types`, label: t("data_tab_broker_account_types") },
+        { id: "brokers", href: `/${basicPath}/brokers`, label: t("data_tab_brokers") },
+        { id: "crypto_providers", href: `/${basicPath}/crypto_providers`, label: t("data_tab_crypto_providers") }
+    ];
+};

453448a7366a81ba19650ad70279fdf2b4c00a78
tothevoid <tothevoid42@gmail.com>
2026-08-14 01:26:50 +0300

    actualized combo box styles

--- client/src/shared/components/CollectionSelect/CollectionSelect.tsx
+++ client/src/shared/components/CollectionSelect/CollectionSelect.tsx
@@ -22,6 +22,41 @@ const CollectionSelect = <T, TFieldValues extends FieldValues>({ name, placehold
         render={({ field }) => (
             <AnySelect
                 {...field}
+                chakraStyles={{
+                    control: (provided: any) => ({
+                        ...provided,
+                        backgroundColor: "background_primary",
+                        borderColor: "border_primary",
+                        color: "text_primary",
+                        _hover: {
+                            borderColor: "border_primary",
+                        },
+                    }),
+                    option: (provided: any, state: any) => ({
+                        ...provided,
+                        color: state.isSelected ? "white" : "text_primary",
+                        backgroundColor: state.isSelected
+                            ? "action_primary"
+                            : state.isFocused
+                                ? "background_secondary"
+                                : "background_primary",
+                    }),
+                    singleValue: (provided: any) => ({
+                        ...provided,
+                        color: "text_primary",
+                    }),
+                    menuList: (provided: any) => ({
+                        ...provided,
+                        backgroundColor: "background_primary",
+                        borderColor: "border_primary",
+                        boxShadow: "md",
+                        borderRadius: "8px",
+                    }),
+                    placeholder: (provided: any) => ({
+                        ...provided,
+                        color: "gray.500",
+                    }),
+                }}
                 getOptionLabel={labelSelector}
                 getOptionValue={valueSelector}
                 options={collection}
